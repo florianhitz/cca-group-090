01848950d5c32211196a0ade5d81e5e4d08c626d
Mischter-X <florian.hitz@hotmail.com>
2025-03-30 14:54:00 +0200

    part2a

---
+++ part2/inspect-run2a.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+INTERFERENCES=("cpu" "l1d" "l1i" "l2" "llc" "membw")
+PARSEC_JOBS=("blackscholes" "canneal" "dedup" "ferret" "freqmine" "radix" "vips")
+
+#none_real=()
+#for job in ${PARSEC_JOBS[@]}
+#do
+#    real=$(sed -n '/^real/p' results-part2a/${job}_none.log)
+#    time_only=$(echo "$real" | grep -o "[0-9]\+m[0-9.]\+s")
+#    min=${time_only%%m*}
+#    sec=${time_only#*m}
+#    sec=${sec%s}
+#    total=$(echo "$min * 60 + $sec" | bc)
+#    none_real+=("$total")
+#done
+#
+#echo "${none_real[@]}"
+     
+
+for job in ${PARSEC_JOBS[@]}
+do
+    real=$(sed -n '/^real/p' results-part2a/${job}_none.log)
+    time_only=$(echo "$real" | grep -o "[0-9]\+m[0-9.]\+s")
+    min=${time_only%%m*}
+    sec=${time_only#*m}
+    sec=${sec%s}
+    total=$(echo "$min * 60 + $sec" | bc -l)
+#    none_real+=("$total")
+
+    for interference in ${INTERFERENCES[@]}
+    do
+        real=$(sed -n '/^real/p' results-part2a/${job}_${interference}.log)
+        time_only=$(echo "$real" | grep -o "[0-9]\+m[0-9.]\+s")
+        min=${time_only%%m*}
+        sec=${time_only#*m}
+        sec=${sec%s}
+        total2=$(echo "$min * 60 + $sec" | bc -l)
+        normalized=$(echo "$total2 / $total" | bc -l)
+        rounded=$(printf "%.2f" "$normalized")
+        
+        if (( $(echo "$rounded <= 1.3" | bc -l) )); then
+            color="green"
+        elif (( $(echo "$rounded <= 2.0" | bc -l) )); then
+            color="YellowOrange"
+        else
+            color="red"
+        fi
+            
+        
+        echo "$job $interference $normalized $color"
+    done
+done
--- part2/run-part2a.sh
+++ part2/run-part2a.sh
@@ -4,17 +4,23 @@ INTERFERENCES=("none" "cpu" "l1d" "l1i" "l2" "llc" "membw")
 PARSEC_JOBS=("blackscholes" "canneal" "dedup" "ferret" "freqmine" "radix" "vips")
 
 for interference in "${INTERFERENCES[@]}"; do
-    if [[ "$interference" != "none" ]]; then
+    if [ "$interference" != "none" ]; then
         echo "Launching iBench interference: $interference"
-        kubectl create -f cloud-comp-arch-project/interference/ibench-${interference}.yaml
-        sleep 10
+
+        kubectl create -f ./interference/ibench-${interference}.yaml
+
+        while [ "$(kubectl get pod ibench-${interference} -o jsonpath='{.status.containerStatuses[0].ready}')" != 'true' ]; do
+            echo "Waiting for $interference to start..."
+            sleep 5
+        done
+        echo "$interference started."
     fi
 
     for job in "${PARSEC_JOBS[@]}"; do
         echo "Running PARSEC job: $job under interference: $interference"
-        kubectl create -f cloud-comp-arch-project/parsec-benchmarks/part2a/parsec-${job}.yaml
+        kubectl create -f ./parsec-benchmarks/part2a/parsec-${job}.yaml
         
-        while [[ $(kubectl get job parsec-${job} -o jsonpath='{.status.succeeded}') != "1" ]]; do
+        while [ "$(kubectl get job parsec-${job} -o jsonpath='{.status.succeeded}')" != "1" ]; do
             echo "Waiting for $job under $interference to finish..."
             sleep 5
         done
@@ -25,8 +31,8 @@ for interference in "${INTERFERENCES[@]}"; do
         kubectl logs "$pod_name" > results-part2a/${job}_${interference}.log
 
         kubectl delete jobs --all
-        kubectl delete pods --all
     done
+    kubectl delete pods --all
 done
         
 

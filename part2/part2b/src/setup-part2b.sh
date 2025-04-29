4b0dbc78546790097cb28f17250c2c76140ec3f1
Weiran Wang <weirwang@ethz.ch>
2025-04-29 16:29:23 +0200

    Make a tiny change on part 2 for the writing of report

--- part2/part2b/src/setup-part2b.sh
+++ part2/part2b/src/setup-part2b.sh
@@ -1,6 +1,8 @@
-export KOPS_STATE_STORE=gs://cca-eth-2025-group-090-flhitz/
+# Promote the user for ETHz ID
+read -p "Enter your ETH ID: " ETHID
+export KOPS_STATE_STORE=gs://cca-eth-2025-group-090-$ETHID/
 PROJECT=`gcloud config get-value project`
-kops create -f ../../part2b.yaml
+kops create -f ../instruction-yaml/part2b.yaml
 kops update cluster part2b.k8s.local --yes --admin
 kops validate cluster --wait 10m
 kubectl get nodes -o wide

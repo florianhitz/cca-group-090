47f4acf3bb95dfa1532934f9af00a201692378a3
Weiran Wang <weirwang@ethz.ch>
2025-05-10 19:28:50 +0200

    Add the part to change the ETHID in yaml file

--- part3/src/setup-part3.sh
+++ part3/src/setup-part3.sh
@@ -1,6 +1,11 @@
 # Promote the user for ETHz ID
 read -p "Enter your ETH ID: " ETHID
 export KOPS_STATE_STORE=gs://cca-eth-2025-group-090-$ETHID/
+# Replace line 16 in the YAML file
+yaml_file="../instruction-yaml/part3.yaml"
+new_line="  configBase: gs://cca-eth-2025-group-090-$ETHID/part4.k8s.local"
+# Replace line 16 (line numbers start at 1)
+sed -i '' "16s|.*|$new_line|" "$yaml_file"
 PROJECT=`gcloud config get-value project`
 kops create -f ../instruction-yaml/part3.yaml
 kops update cluster part3.k8s.local --yes --admin
